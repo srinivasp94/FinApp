848b868cb6ebe4137144d2fcc458e398498fd4bd
srinivaspudi <srinivaspudi@customerinspire.com>
2021-07-05 12:05:01 +0530

    layout changes

--- app/src/main/java/com/sassaran/finapp/Activities/Home Activity.kt
+++ app/src/main/java/com/sassaran/finapp/Activities/Home Activity.kt
@@ -5,7 +5,9 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.sassaran.finapp.R
 import com.sassaran.finapp.base.BaseActivity
+import com.sassaran.finapp.base.GlobalRecyclerAdapter
 import com.sassaran.finapp.databinding.ActivityHomeScreenBinding
+import com.sassaran.finapp.databinding.DummyRecyclerItemBinding
 
 class Home_Activity : BaseActivity<ActivityHomeScreenBinding>() {
 
@@ -19,6 +21,27 @@ class Home_Activity : BaseActivity<ActivityHomeScreenBinding>() {
     }
 
     override fun initialise() {
+
+        textnames.add("Hi ")
+        textnames.add("Hello ")
+        textnames.add("How ")
+        textnames.add("are ")
+        textnames.add("you ")
+        textnames.add("..? ")
+
+        var dummyitemAdapter : GlobalRecyclerAdapter<*,*>  = object :  GlobalRecyclerAdapter<String,DummyRecyclerItemBinding>(textnames){
+            override fun getLayoutId(): Int {
+                return R.layout.dummy_recycler_item;
+            }
+
+            override fun onBinder(model: String, viewbinding: DummyRecyclerItemBinding, position: Int) {
+                viewbinding.txtDummyItem.text = model;
+            }
+
+        }
+
+        binding.rvDummyText.adapter = dummyitemAdapter
+
 
     }
 
